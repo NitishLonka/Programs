1ea54e40d4b645fac8f980c77ea90b30c9b89590
Nitish Lonka <nitishlonka13@gmail.com>
2022-05-19 16:05:24 -0400

    NumberofConnectedComponentsinGraph

---
+++ ds/src/ds/NumberofConnectedComponentsinGraph.java
@@ -0,0 +1,56 @@
+package ds;
+import java.util.*; 
+
+public class NumberofConnectedComponentsinGraph {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int size=7;
+	ArrayList<ArrayList<Integer>> gph = new ArrayList<ArrayList<Integer>>(7);
+	for(int i=0;i<7;i++)
+	{
+		gph.add(new ArrayList<Integer>());
+	}
+	addEdge(gph,0,1); 
+	addEdge(gph,0,2); 
+	addEdge(gph,2,3); 
+	addEdge(gph,1,3); 
+	addEdge(gph,4,5);
+	addEdge(gph,5,6);
+	addEdge(gph,4,6);
+    System.out.print( "Number of Connected Components in Graph: "+BFSTraversal(gph,size));
+	}
+    public static void addEdge(ArrayList<ArrayList<Integer>> gph,int u , int v)
+    {
+    	gph.get(u).add(v);
+    	gph.get(v).add(u);
+    }
+	public static int BFSTraversal(ArrayList<ArrayList<Integer>> gph,int size) {
+		int count=0;
+		boolean[] visited = new boolean[size];
+		 for(int i=0;i<visited.length;i++)
+		 {
+			 visited[i]=false;
+		 }
+		Queue<Integer> q = new LinkedList();
+		for(int i=0;i<size;i++) {
+		if(visited[i]==false) {
+		count++;
+		q.add(i);
+		visited[i] = true;
+		while(q.isEmpty()==false)
+		{
+			Integer x = q.poll();
+			for (Integer k : gph.get(x)) {
+				if(visited[k]!=true) {
+					visited[k] = true;
+				q.add(k);
+				}
+			}
+		}
+		}
+		}
+		return count;
+	}
+}
+
